1df558899fc31015d8e431a6603ae9c369fede31
kevin7520 <kevin7520@hotmail.com>
2024-07-06 22:06:25 -0500

    Correcion en datos de cliente y ordenamiente del mas reciente al mas antiguo

--- TekaApi/TekaApi/obj/Debug/net8.0/TekaApi.AssemblyInfo.cs
+++ TekaApi/TekaApi/obj/Debug/net8.0/TekaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TekaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+216d0f97d4f6bcd5f6408e7d5b780f127c268fc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc6acd92c8ff2641e7d38c73cb6e71b0d867c0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TekaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TekaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
